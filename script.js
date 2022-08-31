4a3942d57330d2b2acaa4751746c72598ee79578
sinahuss <hussaini.sina@gmail.com>
2022-08-31 15:31:53 -0700

    Add button functionality

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 const container = document.querySelector('.container');
-var ROWS = prompt('number');
+const sizeButton = document.getElementById('size');
+var ROWS = 8;
 
+for (let i = 0; i < ROWS * ROWS; i++) {
+    const gridPoint = document.createElement('div');
+    container.append(gridPoint);
+    gridPoint.classList.add('off');
+}
 
 container.style.gridTemplateColumns = 'repeat(' + ROWS + ', auto)';
 
-for (let i = 0; i < ROWS; i++) {
-    for (let j = 0; j < ROWS; j++) {
+//prompt('number');
+
+var gridPoints = document.querySelectorAll('.container > div');
+
+sizeButton.addEventListener('click', () => {
+
+    ROWS = prompt('number');
+    gridPoints.forEach(gridPoint => {
+        container.removeChild(gridPoint);
+    })
+
+    for (let i = 0; i < ROWS * ROWS; i++) {
         const gridPoint = document.createElement('div');
         container.append(gridPoint);
         gridPoint.classList.add('off');
     }
-}
 
-const gridPoints = document.querySelectorAll('.container > div');
+    container.style.gridTemplateColumns = 'repeat(' + ROWS + ', auto)';
+})
 
-gridPoints.forEach((gridPoint) => {
+
+
+
+
+gridPoints.forEach(gridPoint => {
 
     gridPoint.addEventListener('mouseenter', () => {
         gridPoint.classList.toggle('on');
+        gridPoint.classList.toggle('off');
     })
     gridPoint.addEventListener('mouseleave', () => {
         gridPoint.classList.toggle('on');
+        gridPoint.classList.toggle('off');
     })
 })
 
-console.log(window.getComputedStyle(container).width);
+//console.log(window.getComputedStyle(container).width);
