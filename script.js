a9aa1bbc1040f00bc26977d749da0173ba9ee606
sinahuss <hussaini.sina@gmail.com>
2022-08-31 21:05:04 -0700

    Enable feature that darkens each square with each mouseover

--- script.js
+++ script.js
@@ -2,25 +2,32 @@ const container = document.querySelector('.container');
 const sizeButton = document.getElementById('size');
 var ROWS = 4;
 
+//8070ac
+
 function setGrid() {
     
     for (let i = 0; i < ROWS * ROWS; i++) {
-        const gridPoint = document.createElement('div');
-        gridPoint.classList.add('off');
 
-        gridPoint.addEventListener('mouseenter', () => {
-            gridPoint.classList.toggle('on');
-            gridPoint.classList.toggle('off');
-        });
-        gridPoint.addEventListener('mouseleave', () => {
-            gridPoint.classList.toggle('on');
-            gridPoint.classList.toggle('off');
-        });
+        const gridPoint = document.createElement('div');
+
+        gridPoint.classList.add('gridPoint');
+        gridPoint.style.filter = 'brightness(0) invert(1)';
 
         container.append(gridPoint);
+
+        let brightness = 0;
+        gridPoint.addEventListener('mouseenter', () => {
+            brightness += 0.1;
+            gridPoint.style.filter = 'brightness(' + brightness + ') invert(1)';
+            console.log(gridPoint.style.filter[11]);
+        });
     }
 
     container.style.gridTemplateColumns = 'repeat(' + ROWS + ', auto)';
+}
+
+function hover() {
+
 }
 
 setGrid();
